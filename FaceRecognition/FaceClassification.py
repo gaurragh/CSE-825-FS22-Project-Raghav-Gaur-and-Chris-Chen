9a9f24848ae12ccc29cf8b2b8641e4dbaa084461
Christopher Chen <35619672+Maxsito@users.noreply.github.com>
2022-12-05 14:31:57 -0500

    Updated Face Classification with clarity on spacing

--- FaceRecognition/FaceClassification.py
+++ FaceRecognition/FaceClassification.py
@@ -9,23 +9,29 @@ from sklearn.svm import SVC
 data = load('5-celebrity-faces-embeddings.npz')
 trainX, trainy, testX, testy = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
 print('Dataset: train=%d, test=%d' % (trainX.shape[0], testX.shape[0]))
+
 # normalize input vectors
 in_encoder = Normalizer(norm='l2')
 trainX = in_encoder.transform(trainX)
 testX = in_encoder.transform(testX)
+
 # label encode targets
 out_encoder = LabelEncoder()
 out_encoder.fit(trainy)
 trainy = out_encoder.transform(trainy)
 testy = out_encoder.transform(testy)
+
 # fit model
 model = SVC(kernel='linear', probability=True)
 model.fit(trainX, trainy)
+
 # predict
 yhat_train = model.predict(trainX)
 yhat_test = model.predict(testX)
+
 # score
 score_train = accuracy_score(trainy, yhat_train)
 score_test = accuracy_score(testy, yhat_test)
+
 # summarize
 print('Accuracy: train=%.3f, test=%.3f' % (score_train*100, score_test*100))
